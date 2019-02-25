337c121f92d22583b85c60b471bf459764490f8e
7429138 <liam.bell@sdsmt.edu>
2019-02-24 21:15:59 -0700

    Improved Podcast class and added Episode class

--- lib/WebPodcasts/itunes_podcast.dart
+++ lib/WebPodcasts/itunes_podcast.dart
@@ -8,26 +8,73 @@ class ItunesPodcast
   Future<http.Response> resp;
   dom.Document doc;
   String location;
+  String title;
   Image art;
   String description;
-  List<dom.Element> episodes;
+  List<Episode> episodes = List<Episode>();
 
-  ItunesPodcast(String location)
+  addEpisode(dom.Element e)
   {
-    location = location;
+    String link = e.attributes['audio-preview-url'];
+    String name = e.attributes['preview-title'];
+
+    String desc = e.getElementsByClassName('description flexible-col').first
+      .getElementsByClassName('text').first.text;
+    int nmbr = int.parse(
+      e.getElementsByClassName('index ascending').first.getElementsByTagName('span').first.text
+    );
+
+    Duration dur = Duration( milliseconds:
+      (e.attributes['preview-duration']==null)?0:int.parse(e.attributes['preview-duration'])
+    );
+
+    String rel = e.getElementsByClassName('release-date').first.getElementsByClassName('text').first.text;
+    DateTime released = DateTime(
+      int.parse(rel.split('/')[2]), // year
+      int.parse(rel.split('/')[0]), // month
+      int.parse(rel.split('/')[1]), // day
+    );
+
+    episodes.add( Episode(link, art, name, desc, nmbr, dur, released) );
+  }
+
+  ItunesPodcast(this.location)
+  {
     resp = http.get(location).then((r){
       doc = parse(r.body);
-      // art = Image.network(doc.getElementsByClassName('artwork').first.attributes['src']);
+      title = doc.getElementById('title').getElementsByTagName('h1').first.text;
+      art = Image.network(doc.getElementsByClassName('artwork').first.getElementsByClassName('artwork').first.attributes['src']);
       description = doc.getElementsByClassName('product-review').first.getElementsByTagName('p').first.text;
-      episodes = doc.getElementsByClassName('podcast-episode');
+      doc.getElementsByClassName('podcast-episode').forEach(addEpisode);
     });
   }
 
   void update()
   {
     description = doc.getElementsByClassName('product-review').first.getElementsByTagName('p').first.text;
-    episodes = doc.getElementsByClassName('podcast-episode');
-    print(description);
+    doc.getElementsByClassName('podcast-episode').forEach(addEpisode);
+    print(title);
   }
 
+}
+
+class Episode
+{
+  String link;
+  Image art;
+  String name;
+  String description;
+  int number;
+  Duration duration;
+  DateTime released;
+
+  Episode(
+    this.link,
+    this.art,
+    this.name,
+    this.description,
+    this.number,
+    this.duration,
+    this.released,
+  );
 }
