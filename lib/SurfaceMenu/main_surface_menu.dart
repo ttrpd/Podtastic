fcb04c45f8a25638012fe107b9d0743dbd4ab646
7429138 <liam.bell@sdsmt.edu>
2019-09-17 16:52:58 -0600

    Readability

--- lib/SurfaceMenu/main_surface_menu.dart
+++ lib/SurfaceMenu/main_surface_menu.dart
@@ -1,10 +1,8 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart' as prefix0;
-import 'package:podtastic/SurfaceMenu/MyPodcastsPage/my_podcasts_page.dart';
-import 'package:podtastic/SurfaceMenu/menu_item.dart';
+import 'package:podtastic/SurfaceMenu/NowPlayingPage/now_playing_info.dart';
 import 'package:podtastic/SurfaceMenu/SearchPage/search_page.dart';
+import 'package:podtastic/SurfaceMenu/podcast_menu.dart';
 
 class MainSurfaceMenu extends StatefulWidget {
 
@@ -90,7 +88,13 @@ class _MainSurfaceMenuState extends State<MainSurfaceMenu> with TickerProviderSt
       backgroundColor: Theme.of(context).backgroundColor,
       body: Stack(
         children: <Widget>[
-          buildMenu(),
+          PodcastMenu(
+            context: context,
+            surfaceDrawerPage: surfaceDrawerPage,
+            onMenuTap: onMenuTap,
+            onNowPlayingTap: onNowPlayingTap,
+            setSurfaceDrawerPage: (w)=> surfaceDrawerPage = w,
+          ),
           Stack(
             children: <Widget>[
               SurfaceDrawer(
@@ -103,214 +107,15 @@ class _MainSurfaceMenuState extends State<MainSurfaceMenu> with TickerProviderSt
                 surfaceDrawerHeight: MediaQuery.of(context).size.height,
                 slidePercent: slidePercent,
                 bottomPage: surfaceDrawerPage,
-                topPage: buildNowPlayingInfo(context, showNowPlaying),
+                topPage: NowPlayingInfo(
+                  showNowPlaying: showNowPlaying,
+                  onBackArrowTap: onNowPlayingTap,
+                ),
               ),
             ],
           ),
         ],
       )
-    );
-  }
-
-  Widget buildNowPlayingInfo(BuildContext context, bool showNowPlaying) {
-    return AnimatedOpacity(
-      opacity: showNowPlaying ? 1.0 : 0.0,
-      duration: Duration(milliseconds: 170),
-      child: Container(
-        height: double.maxFinite,
-        width: double.maxFinite,
-        color: Theme.of(context).primaryColor,
-        child: Padding(
-          padding: EdgeInsets.only(top: (MediaQuery.of(context).size.height/2.6)),
-          child: Container(
-            color: Theme.of(context).primaryColor,
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 15.0),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Container(
-                          alignment: Alignment.centerLeft,
-                          child: IconButton(
-                            icon: Icon(Icons.arrow_back_ios, color: Theme.of(context).backgroundColor,),
-                            color: Colors.transparent,
-                            onPressed: onNowPlayingTap,
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          alignment: Alignment.centerRight,
-                          child: IconButton(
-                            icon: Icon(Icons.settings, color: Theme.of(context).backgroundColor,),
-                            color: Colors.transparent,
-                            onPressed: onNowPlayingTap,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    child: PageView(
-                      children: <Widget>[
-                        Column(
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.only(top: 8.0, left: 8.0, bottom: 25.0, right: 8.0),
-                              child: Container(
-                                alignment: Alignment.center,
-                                width: double.maxFinite,
-                                child: RichText(
-                                  text: TextSpan(
-                                    text: "Test Title",
-                                    style: Theme.of(context).primaryTextTheme.title
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Divider(
-                              color: Theme.of(context).primaryTextTheme.title.color,
-                              indent: 100.0,
-                              endIndent: 100.0,
-                            ),
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.only(left: 25.0, right: 25.0, bottom: 30.0, top: 25.0),
-                                child: RichText(
-                                  text: TextSpan(
-                                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sed viverra ipsum nunc aliquet bibendum enim. In massa tempor nec feugiat. Nunc aliquet bibendum enim facilisis gravida. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Amet luctus venenatis lectus magna fringilla. Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Sagittis orci a scelerisque purus semper eget duis. Nulla pharetra diam sit amet nisl suscipit. Sed adipiscing",
-                                    style: Theme.of(context).primaryTextTheme.body1
-                                  ),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              color: Colors.blueAccent,
-                              height: MediaQuery.of(context).size.height/7,
-                            ),
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(60.0),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(15.0),
-                            child: Container(
-                              width: MediaQuery.of(context).size.width / 1.5,
-                              height: MediaQuery.of(context).size.width / 1.5,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  SafeArea buildMenu() {
-    return SafeArea(
-      child: Container(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 7.0),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.centerLeft,
-                      child: IconButton(
-                        icon: Icon(Icons.menu, color: Theme.of(context).primaryColor,),
-                        color: Colors.transparent,
-                        onPressed: onMenuTap,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.center,
-                      child: RichText(
-                        text: TextSpan(
-                          text: 'Search',
-                          style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                            fontSize: 24,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.centerRight,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: IconButton(
-                          icon: Icon(Icons.headset, color: Theme.of(context).primaryColor,), 
-                          color: Colors.transparent,
-                          onPressed: onNowPlayingTap
-                        ),
-                      )
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  MenuItem(
-                    text: 'Search',
-                    enabled: true,
-                    selected: surfaceDrawerPage is SearchPage,
-                    onPressed: (){
-                      surfaceDrawerPage = SearchPage();
-                      onMenuTap();
-                    },
-                  ),
-                  MenuItem(
-                    text: 'Downloads',
-                    enabled: false,
-                    selected: false,
-                    onPressed: (){},
-                  ),
-                  MenuItem(
-                    text: 'My Podcasts',
-                    enabled: true,
-                    selected: surfaceDrawerPage is MyPodcastsPage,
-                    onPressed: (){
-                      surfaceDrawerPage = MyPodcastsPage();
-                      onMenuTap();
-                    },
-                  ),
-                  MenuItem(
-                    text: 'Settings',
-                    enabled: false,
-                    selected: false,
-                    onPressed: (){},
-                  ),
-                  Container(//for spacing purposes
-                    height: 100.0,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
@@ -319,7 +124,7 @@ class SurfaceDrawer extends StatefulWidget {
   const SurfaceDrawer({
     Key key,
     @required this.bottomPage,
-    @required this.topPage,
+    this.topPage,
     @required this.surfaceDrawerHeight,
     @required this.slidePercent,
     this.initialDisplacement = 100.0,
---
+++ lib/SurfaceMenu/podcast_menu.dart
@@ -0,0 +1,125 @@
+
+import 'package:flutter/material.dart';
+import 'package:podtastic/SurfaceMenu/MyPodcastsPage/my_podcasts_page.dart';
+import 'package:podtastic/SurfaceMenu/SearchPage/search_page.dart';
+import 'package:podtastic/SurfaceMenu/menu_item.dart';
+
+class PodcastMenu extends StatefulWidget {
+  PodcastMenu({
+    Key key,
+    @required this.context,
+    @required this.surfaceDrawerPage,
+    @required this.setSurfaceDrawerPage,
+    @required this.onNowPlayingTap,
+    @required this.onMenuTap,
+  }) : super(key: key);
+
+  final BuildContext context;
+  final void Function(Widget) setSurfaceDrawerPage;
+  final Widget surfaceDrawerPage;
+  final void Function() onNowPlayingTap;
+  final void Function() onMenuTap;
+
+  @override
+  _PodcastMenuState createState() => _PodcastMenuState();
+}
+
+class _PodcastMenuState extends State<PodcastMenu> {
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 7.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.centerLeft,
+                      child: IconButton(
+                        icon: Icon(Icons.menu, color: Theme.of(context).primaryColor,),
+                        color: Colors.transparent,
+                        onPressed: widget.onMenuTap,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'Search',
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.centerRight,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: IconButton(
+                          icon: Icon(Icons.headset, color: Theme.of(context).primaryColor,), 
+                          color: Colors.transparent,
+                          onPressed: widget.onNowPlayingTap
+                        ),
+                      )
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  MenuItem(
+                    text: 'Search',
+                    enabled: true,
+                    selected: widget.surfaceDrawerPage is SearchPage,
+                    onPressed: (){
+                      widget.setSurfaceDrawerPage(SearchPage());
+                      widget.onMenuTap();
+                    },
+                  ),
+                  MenuItem(
+                    text: 'Downloads',
+                    enabled: false,
+                    selected: false,
+                    onPressed: (){},
+                  ),
+                  MenuItem(
+                    text: 'My Podcasts',
+                    enabled: true,
+                    selected: widget.surfaceDrawerPage is MyPodcastsPage,
+                    onPressed: (){
+                      widget.setSurfaceDrawerPage(MyPodcastsPage());
+                      widget.onMenuTap();
+                    },
+                  ),
+                  MenuItem(
+                    text: 'Settings',
+                    enabled: false,
+                    selected: false,
+                    onPressed: (){},
+                  ),
+                  Container(//for spacing purposes
+                    height: 100.0,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
